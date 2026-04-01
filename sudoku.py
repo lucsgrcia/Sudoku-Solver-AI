918a93bfb50bf8df88bbe0ab44eb783dc5a4d094
Lucas Garcia <lucas.garciadelacerda@gmail.com>
2026-04-01 16:06:25 -0300

    add legenda

--- sudoku.py
+++ sudoku.py
@@ -147,6 +147,7 @@ def solution(grid_param):
       return
 
 # Para rodar o exemplo difícil é necessário esperar cerca de 1 minuto, talvez até um pouco mais
+print("\n\nSUDOKU RESOLVIDO:\n")
 grid_str = "4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......"
 #grid_str = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
 solved_board = solution(grid_str)
